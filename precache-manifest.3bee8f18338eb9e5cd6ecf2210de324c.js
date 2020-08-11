6a724469b9b8aad996f665c21fcde003c5e3d51d
Jesus Chacon <chjesussao@gmail.com>
2020-08-11 01:38:44 -0400

    Updates

---
+++ precache-manifest.3bee8f18338eb9e5cd6ecf2210de324c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2046c3d5067bb5b5e9cca60b91c333e",
+    "url": "/search-user-github/index.html"
+  },
+  {
+    "revision": "8439a7cefda9c1610f92",
+    "url": "/search-user-github/static/css/2.ed7b5974.chunk.css"
+  },
+  {
+    "revision": "dac0c2c0f6d8b9217e30",
+    "url": "/search-user-github/static/css/main.01a5bcca.chunk.css"
+  },
+  {
+    "revision": "8439a7cefda9c1610f92",
+    "url": "/search-user-github/static/js/2.40c31118.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/search-user-github/static/js/2.40c31118.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dac0c2c0f6d8b9217e30",
+    "url": "/search-user-github/static/js/main.a4f1ca40.chunk.js"
+  },
+  {
+    "revision": "814b0fb342e7d279530e",
+    "url": "/search-user-github/static/js/runtime-main.f0204a2b.js"
+  }
+]);
